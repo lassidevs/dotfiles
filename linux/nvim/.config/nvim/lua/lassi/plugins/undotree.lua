88627cbc8bf2bbff2b3541e74359f05283f24fec
lassidevs <lassi.koivisto7@gmail.com>
2025-07-30 03:05:57 +0300

    add simple dotfiles

---
+++ linux/nvim/.config/nvim/lua/lassi/plugins/undotree.lua
@@ -0,0 +1,27 @@
+return {
+	"mbbill/undotree",
+
+	keys = {
+		{
+			"<leader><F5>",
+			function()
+				vim.cmd.UndotreeToggle()
+			end,
+			desc = "Toggle Undotree",
+		},
+	},
+
+	config = function()
+		-- Persistent undo setup
+		if vim.fn.has("persistent_undo") == 1 then
+			local target_path = vim.fn.expand("~/.undodirr")
+
+			if vim.fn.isdirectory(target_path) == 0 then
+				vim.fn.mkdir(target_path, "p", 448)
+			end
+
+			vim.o.undodir = target_path
+			vim.o.undofile = true
+		end
+	end,
+}
