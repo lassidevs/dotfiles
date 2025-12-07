dde5371a0927dd6153c35fc664e86a44ed42a12f
lassidevs <lassi.koivisto7@gmail.com>
2025-12-07 13:16:51 +0200

    feat: telescope home dir

--- linux/nvim/.config/nvim/lua/lassi/plugins/telescope.lua
+++ linux/nvim/.config/nvim/lua/lassi/plugins/telescope.lua
@@ -42,6 +42,13 @@ return {
 		local keymap = vim.keymap -- for conciseness
 
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fg", function()
+			require("telescope.builtin").find_files({
+				cwd = vim.fn.expand("~"), -- Set to $HOME
+				hidden = true, -- Include hidden files like .config
+				follow = true, -- Follow symlinks
+			})
+		end, { desc = "Fuzzy find files in $HOME" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
